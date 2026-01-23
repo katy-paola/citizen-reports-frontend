566ca6f94a0f009b048bb42fa5c8c2e8301d550c
katy-paola <kbarbozab@unicartagena.edu.co>
2026-01-22 22:03:32 -0500

    test: add logs to show how backend is sending totalPages in prod

--- src/shared/components/ReportsPagination.tsx
+++ src/shared/components/ReportsPagination.tsx
@@ -3,9 +3,12 @@ import Button from "./Button";
 
 export default function ReportsPagination() {
   const { pagination, isLoading, isError } = usePaginatedReports();
+  console.log("pagination inside of ReportsPagination component:", pagination);
   if (isLoading || isError || !pagination) return null;
   const { page, totalPages, reportsToShow, totalReports, handlePageChange } =
     pagination;
+
+  console.log("totalPages inside of ReportsPagination component", totalPages);
 
   return (
     <div className="pagination">
